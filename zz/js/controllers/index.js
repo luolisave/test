c20f487361493b0438a8b29a02dbf515140e3a63
Li Luo <luolisave@gmail.com>
2015-06-21 14:57:02 +0000

    init

--- zz/js/controllers/index.js
+++ zz/js/controllers/index.js
@@ -4,24 +4,54 @@
 app.controller('IndexCtrl', ['$scope','$interval',function IndexCtrl($scope,$interval) {
 	//0. Init.
 	$scope.allTimes = {
-		toronto:54087,
+		system:0,
+		systemMs:0,
+		systemMsPrevious:0,
+		toronto:0,
 		torontoHms:'',
 		london:0,
 		londonHms:'',
+		londonTimezone:-6,
 		sydney:0,
-		sydneyHms:''
+		sydneyHms:'',
+		sydneyTimezone:-13
 	}
 	
 	//1. functions
 	//1.1 update time function
 	$scope.updateTime = function(){
-		//var tmpTorontoSecond = Hms2seconds($scope.torontoTimeSetup);
-		if(angular.isNumber($scope.allTimes.toronto)){
-			console.log($scope.allTimes.toronto);
-			$scope.torontoHms = Seconds2hms($scope.allTimes.toronto);
-			console.log($scope.torontoHms);
+		$scope.allTimes.systemMsPrevious = $scope.allTimes.systemMs;
+		$scope.allTimes.systemMs = new Date().getTime();
+		$scope.allTimes.system = $scope.allTimes.systemMs / 1000;
+		$scope.allTimes.toronto += ($scope.allTimes.systemMs - $scope.allTimes.systemMsPrevious)/1000;
+	}
+	//1.2
+	$scope.changeLondonTimezone = function(tz){
+		if(tz && tz>=-23 && tz <=23){
+			$scope.allTimes.londonTimezone = tz;
 		}else{
-			alert("Invalid toronto time.");
+			alert("invalid timezone");
+		}
+	}
+	//1.3
+	$scope.changeSydneyTimezone = function(tz){
+		if(tz && tz>=-23 && tz <=23){
+			$scope.allTimes.sydneyTimezone = tz; 
+		}else{
+			alert("invalid timezone");
+		}
+	}
+	//1.4 
+	$scope.setTorontoTime = function(torontoTime_tmp){
+		try{
+			var torontoT_tmp = Hms2seconds(torontoTime_tmp);
+			if(torontoT_tmp){
+				$scope.allTimes.toronto = torontoT_tmp;
+			}else{
+				alert("Invalid Input.");
+			}
+		}catch(e){
+			alert("Invalid Input.");
 		}
 	}
 	
