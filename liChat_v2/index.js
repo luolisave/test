9845c0b0c98e2be7a0e1394380c43709b45030e9
LiL <lil@sigmasoft.ca>
2016-03-07 16:26:25 -0500

    Change to bootstrap 3 style.

--- liChat_v2/index.js
+++ liChat_v2/index.js
@@ -1,10 +1,13 @@
+var express = require('express');
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/index.html');
-});
+//app.get('/', function (req, res) {
+//    res.sendFile(__dirname + '/index.html');
+//});
+
+app.use(express.static(__dirname + '/public'));
 
 var histories = [];
 io.on('connection', function (socket) {
