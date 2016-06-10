0921a98fee7db2fe4f76b1f6112b65027bc47339
LiL <lil@sigmasoft.ca>
2016-06-10 12:40:02 -0400

    server and client done.

--- nodejs/net/client.js
+++ nodejs/net/client.js
@@ -6,21 +6,17 @@ var PORT = 8107;
 
 var client = new net.Socket();
 client.connect(PORT, HOST, function() {
-
     console.log('CONNECTED TO: ' + HOST + ':' + PORT);
     // Write a message to the socket as soon as the client is connected, the server will receive it as message from the client
-    client.write('I am Chuck Norris!');
-
+    client.write('I am Li!');
 });
 
 // Add a 'data' event handler for the client socket
 // data is what the server sent to this socket
 client.on('data', function(data) {
-
     console.log('DATA: ' + data);
     // Close the client socket completely
     client.destroy();
-
 });
 
 // Add a 'close' event handler for the client socket
--- nodejs/net/server.js
+++ nodejs/net/server.js
@@ -1,5 +1,5 @@
-// net: http://www.hacksparrow.com/tcp-socket-programming-in-node-js.html
-var net = require('net');
+var net = require('net'); // net: http://www.hacksparrow.com/tcp-socket-programming-in-node-js.html
+var fs = require('fs');   // fs:  http://stackoverflow.com/questions/3459476/how-to-append-to-a-file-in-node
 
 var HOST = '127.0.0.1';
 var PORT = 8107;
@@ -14,10 +14,21 @@ net.createServer(function(sock) {
 
     // Add a 'data' event handler to this instance of socket
     sock.on('data', function(data) {
-
         console.log('DATA ' + sock.remoteAddress + ': ' + data);
         // Write the data back to the socket, the client will receive it as data from the server
-        sock.write('You said "' + data + '"');
+        sock.write('Incomming Data is: "' + data + '"');
+        // append incomming data to log.txt file. (with time)
+        var date = new Date();
+        var dateStr = date.toISOString();
+        var tmpMsg = "["+dateStr + "]" + data + "\r\n";
+        fs.appendFile('log.txt', tmpMsg, function (err) {
+          if(err){
+            //put error handler here. 
+            console.log("file not logged, error: ", err);
+          }else{
+
+          }
+        });
 
     });
 
