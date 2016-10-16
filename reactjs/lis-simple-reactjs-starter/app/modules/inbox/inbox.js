c67e4553ed50e1b6a84d4367dc5e021475592c94
Li Luo <luolisave@gmail.com>
2016-10-16 02:53:35 +0000

    add new changes

--- reactjs/lis-simple-reactjs-starter/app/modules/inbox/inbox.js
+++ reactjs/lis-simple-reactjs-starter/app/modules/inbox/inbox.js
@@ -4,8 +4,29 @@ window.Inbox = React.createClass({
   render() {
     return (
       <div>
-        <h2>Inbox</h2>
-        {this.props.children || "Welcome to your Inbox"}
+        <div className="row">
+          <div className="col-sm-12">
+            <h2>Inbox</h2>
+          </div>
+        </div>
+        <div className="row">
+          
+          <div className="col-sm-2">
+            <h3>Mails:</h3>
+            <ul>
+              <li><a href="#/inbox/messages/1">messages 1</a></li>
+              <li><a href="#/inbox/messages/2">messages 2</a></li>
+              <li><a href="#/inbox/messages/3">messages 3</a></li>
+              <li><a href="#/inbox/messages/4">messages 4</a></li>
+              <li><a href="#/inbox/messages/5">messages 5</a></li>
+            </ul>
+          </div>
+          <div className="col-sm-10">
+            {this.props.children || "Welcome to your Inbox. Current Inbox is empty!"}
+          </div>
+          
+          
+        </div>
       </div>
     )
   }
