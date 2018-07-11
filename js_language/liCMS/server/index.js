8874cdd5e60567c8e1e621a8c4ecf86704e360fd
luolisave <luolisave@gmail.com>
2018-07-11 15:05:58 -0400

    add pages database.

--- js_language/liCMS/server/index.js
+++ js_language/liCMS/server/index.js
@@ -9,10 +9,15 @@ var Datastore = require('nedb')
 var db = {}
 db.users = new Datastore({ filename: path.join(__dirname, 'private/nedb/users') });
 db.users.loadDatabase();
+db.pages = new Datastore({ filename: path.join(__dirname, 'private/nedb/pages') });
+db.pages.loadDatabase();
 
+// var tmpPassword = 'pass1234';
 // var doc = { username: 'admin'
-//             , role: 'admin'
-//                , password: '12345'
+//                , role: 'admin'
+//                , password: 'pass1234'
+//                , token: 'SDJIOQDJL32432DWIDJSKWDJKDS'
+//                , tokenTime: 1531235997
 //                , updateDate: new Date()
 //                , status: 'active'
 //                };
@@ -40,6 +45,41 @@ app.post('/api/v1/user/login', (req, res) => {
     res.setHeader('Content-Type', 'application/json');
     res.send(JSON.stringify(req.body));
     // res.send(JSON.stringify({ a: 1 }));
+});
+
+app.get('/api/v1/page/:pageId', (req, res) => {
+    console.log('req.params = ', req.params);
+
+    if(req.params && req.params.pageId !== undefined){
+        db.pages.findOne({ _id: req.params.pageId }, function (err, doc) {
+            // doc is the document Mars
+            // If no document is found, doc is null
+            if(!err){
+                res.setHeader('Content-Type', 'application/json');
+                if(doc){
+                    res.send(JSON.stringify({ status: 1, info: 'good!', data: doc }));
+                }else{
+                    res.send(JSON.stringify({ status: 0, info: 'cannot find doc!', data:{} }));
+                }
+                
+            }else{
+                res.setHeader('Content-Type', 'application/json');
+                res.send(JSON.stringify({ status: 0, info: err, data:{} }));
+            }
+        });
+    }else{
+        res.setHeader('Content-Type', 'application/json');
+        res.send(JSON.stringify({ status: 0, info: 'pageId not exist!', data:{} }));
+    }
+
+    // let pageDoc = {
+    //     pageId : 1,
+    //     pageTitle: 'page title 1',
+    //     pageContent: 'page content 1'
+    // }
+    // db.pages.insert(pageDoc, function (err, pageDoc) {   // Callback is optional
+    //     console.log('inserted:', pageDoc);
+    // });
 });
 
 app.listen(3333, () => console.log('Example app listening on port 3333!'))
