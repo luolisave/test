ec64629e171e1075299d85e9a72871ea37c1d9b6
LUOLI96 <li.luo@td.com>
2018-10-19 16:25:23 -0400

    refactor docs.

--- js_language/liCMS/server/src/mock.js
+++ js_language/liCMS/server/src/mock.js
@@ -1,8 +1,9 @@
 const crud = require('./crud');
+const currentDB = 'mocks';
 
 function appRoute(app, db){
     app.get('/api/v1/mock/list', (req, res) => {
-        let promise = crud.listX(req, res, db, db.mocks, {method:'passcode'});
+        let promise = crud.listX(req, res, db, db[currentDB], {method:'passcode'});
         promise.then(
             function(result){
                 res.setHeader('Content-Type', 'application/json');
@@ -15,7 +16,7 @@ function appRoute(app, db){
     });
 
     app.get('/api/v1/mock/:xId', (req, res) => {
-        let promise = crud.getX(req, res, db, db.mocks, {method:'passcode'});
+        let promise = crud.getX(req, res, db, db[currentDB], {method:'passcode'});
         console.log('---mock');
         promise.then(
             function(result){
@@ -31,7 +32,7 @@ function appRoute(app, db){
     });
 
     app.post('/api/v1/mock/create', (req, res) => {
-        let promise = crud.createX(req, res, db, db.mocks, {method:'passcode'});
+        let promise = crud.createX(req, res, db, db[currentDB], {method:'passcode'});
         promise.then(
             function(result){
                 res.setHeader('Content-Type', 'application/json');
@@ -44,7 +45,7 @@ function appRoute(app, db){
     });
 
     app.patch('/api/v1/mock/:xId', (req, res) => {
-        let promise = crud.updateX(req, res, db, db.mocks, {method:'passcode'});
+        let promise = crud.updateX(req, res, db, db[currentDB], {method:'passcode'});
         promise.then(
             function(result){
                 res.setHeader('Content-Type', 'application/json');
@@ -59,7 +60,7 @@ function appRoute(app, db){
 
     app.delete('/api/v1/mock/:xId', (req, res) => {
         crud.delX(req, res, db, db.mocks, {method:'passcode'});
-        let promise = crud.delX(req, res, db, db.mocks, {method:'passcode'});
+        let promise = crud.delX(req, res, db, db[currentDB], {method:'passcode'});
         promise.then(
             function(result){
                 res.setHeader('Content-Type', 'application/json');
--- js_language/liCMS/server/src/page.js
+++ js_language/liCMS/server/src/page.js
@@ -1,8 +1,9 @@
 const crud = require('./crud');
+const currentDB = 'pages';
 
 function appRoute(app, db){
     app.post('/api/v1/page/create', (req, res) => {
-        let promise = crud.createX(req, res, db, db.pages, {method:'token'});
+        let promise = crud.createX(req, res, db, db[currentDB], {method:'token'});
         promise.then(
             function(result){
                 res.setHeader('Content-Type', 'application/json');
@@ -15,7 +16,7 @@ function appRoute(app, db){
     });
 
     app.get('/api/v1/page/list', (req, res) => {
-        let promise = crud.listX(req, res, db, db.pages, {method:'token'});
+        let promise = crud.listX(req, res, db, db[currentDB], {method:'token'});
         promise.then(
             function(result){
                 res.setHeader('Content-Type', 'application/json');
@@ -28,7 +29,7 @@ function appRoute(app, db){
     });
 
     app.patch('/api/v1/page/:xId', (req, res) => {
-        let promise = crud.updateX(req, res, db, db.pages, {method:'token'});
+        let promise = crud.updateX(req, res, db, db[currentDB], {method:'token'});
         promise.then(
             function(result){
                 res.setHeader('Content-Type', 'application/json');
@@ -41,7 +42,7 @@ function appRoute(app, db){
     });
 
     app.get('/api/v1/page/:xId', (req, res) => {
-        let promise = crud.getX(req, res, db, db.pages, {method:'token'});
+        let promise = crud.getX(req, res, db, db[currentDB], {method:'token'});
         promise.then(
             function(result){
                 res.setHeader('Content-Type', 'application/json');
@@ -54,7 +55,7 @@ function appRoute(app, db){
     });
 
     app.delete('/api/v1/page/:xId', (req, res) => {
-        let promise = crud.delX(req, res, db, db.pages, {method:'token'});
+        let promise = crud.delX(req, res, db, db[currentDB], {method:'token'});
         promise.then(
             function(result){
                 res.setHeader('Content-Type', 'application/json');
