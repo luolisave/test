182450b70bc8aaf0ae41a979b2ee5da4d51ffa24
luolisave <luolisave@gmail.com>
2019-01-26 13:11:17 -0500

    add business mock sample.

--- js_language/liCMS/server/src/mock.js
+++ js_language/liCMS/server/src/mock.js
@@ -2,6 +2,24 @@ const crud = require('./crud');
 const currentDB = 'mocks';
 
 function appRoute(app, db){
+
+    // mock business starts =========================================================================================
+    app.patch('/mock/business', (req, res) => {
+        const mockDB = db['mocks'];
+        mockDB.findOne({ _id: '1lcyijBFZdahhXpQ' }, function (err, doc) {
+            if(!err && doc){
+                res.setHeader('Content-Type', 'application/json');
+                doc.modify = 'modified according to business';
+                res.send(JSON.stringify(doc));
+            }else{
+                res.setHeader('Content-Type', 'application/json');
+                res.send(JSON.stringify({err:err,doc:doc}));
+            }
+        }); 
+    });
+
+    
+    // mock doc management. ===================================================================
     app.get('/mock/list', (req, res) => {
         let promise = crud.listX(req, res, db, db[currentDB], {method:'null'});
         promise.then(
@@ -56,7 +74,6 @@ function appRoute(app, db){
                 res.send(JSON.stringify(err));
             });
     });
-
 
     app.delete('/mock/:xId', (req, res) => {
         crud.delX(req, res, db, db.mocks, {method:'null'});
